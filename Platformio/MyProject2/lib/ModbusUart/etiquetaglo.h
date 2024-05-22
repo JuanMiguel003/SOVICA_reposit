73f9fa23182b10ab839a408048dff5440a056b2e
JuanMiguel003 <jmiguelc97@gmail.com>
2024-05-22 11:54:00 -0400

    Cambios a velocidad sensor 9600'

--- Platformio/MyProject2/lib/ModbusUart/etiquetaglo.h
+++ Platformio/MyProject2/lib/ModbusUart/etiquetaglo.h
@@ -1,6 +1,6 @@
 
 #define MB_NUM_UART     1   // Number of UART port used for Modbus connection
-#define MB_SPEED_BAUDR    (4800)  //4800 The communication speed of the UART
+#define MB_SPEED_BAUDR    (9600)  //4800 The communication speed of the UART
 #define MB_PIN_RX   (23)         //     17verde  
 #define MB_PIN_TX   (22)        //     18blanco
 #define MB_PIN_RTS  (21)         //rts 4         34=rts   // ##comentar si no se utiliza
